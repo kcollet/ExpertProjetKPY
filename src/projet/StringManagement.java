bbc81ce98fd4a6623fabd424d86013fadfa2ac90
nemesis <younesskate@hotmail.fr>
2020-03-06 17:28:35 +0100

    Correction de la fonction eraseLetters et test unitaire effectué

--- src/projet/StringManagement.java
+++ src/projet/StringManagement.java
@@ -1,54 +1,51 @@
 package projet;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class StringManagement {
 	
-	private static int delete_percent = 30;
+	private static final int delete_percent = 30;
 
 	public static void main(String[] args) {
 		
+		Scanner keyboard = new Scanner(System.in);
+		
 		String user_word = "";
 		String filtered_word = "";
-		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Veuillez taper une phrase:");
-		user_word = sc.nextLine();
-		filtered_word = eraseLetters(user_word);
-		System.out.println("Mot corrigé " + filtered_word);
-		sc.close();
+		user_word = keyboard.nextLine();
+		
+		System.out.println("Mot corrigé " + eraseLetters(user_word));
+
 	}
 	
-	public static String eraseLetters(String string) {
-		
+	public static String eraseLetters(String string)
+	{
+		Random random = new Random();
 		int string_size = string.length();
 		char[] chars = string.toCharArray(); // Convert string in char array
+		int[] matrice = new int[string_size];
 		int char_to_delete = (string.length() * delete_percent / 100); // Number of char to delete
-		System.out.println("Characters to delete : " +  char_to_delete);
+		System.out.println(char_to_delete);
 		
-		Random random = new Random();
-		
-		for (int i = 0; i < char_to_delete; i++)
-		{
-			int randomChar = random.nextInt(string_size- i);
+		for (int i = 0; i < matrice.length; i++) {
 			
-			//Debug
-			System.out.println(randomChar);
-			System.out.println(chars);
-			System.out.println("i = " + i);
+			int rand = random.nextInt(2);  // 0 or 1
 			
-			if (chars[randomChar] == ' ') {
-				i --;
+			if (char_to_delete > 0 && rand == 0) {
+				matrice[i] = 0;
+				char_to_delete -= 1;
 			} else {
-				chars[randomChar] = ' '; // Supprime les lettres --> Corriger la fonction pour qu'un même indice
-				//ne puisse pas être selectionner 2x. Faire le truc a l'envers --> Garder 70% des characters.
+				matrice[i] = 1;
 			}
-		
 		}
 		
-		String s = new String(chars);
-		return s;
+		for (int i = 0; i < matrice.length; i++)
+		{	
+			if (matrice[i] ==  0) 
+				chars[i] = '*';
+		}	
+		return new String(chars);	
 	}
-
 }
